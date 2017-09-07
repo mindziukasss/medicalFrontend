f6d8921d097dd9f198a08ecbe26e09a7834989d8
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-09-07 13:39:35 +0300

    fixed route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,9 @@
-import {Routes, RouterModule} from "@angular/router";
-import {AllUsersComponent} from "./admin/users/all-users/all-users.component";
+import {Routes, RouterModule} from '@angular/router';
+import {AllUsersComponent} from './admin/users/all-users/all-users.component';
+import {ModuleWithProviders} from '@angular/core';
 
 const APP_Routes:Routes = [
-    {path: '/admin/users', component: AllUsersComponent}
+    {path: 'admin/users', component: AllUsersComponent}
 ];
 
-export const routing = RouteModule.forRoot(APP_Routes);
+export const routing:ModuleWithProviders = RouterModule.forRoot(APP_Routes);
