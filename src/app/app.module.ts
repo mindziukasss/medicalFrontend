4341b555574d1d7fb4d1b65ab46baca0b81de98c
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-09-07 12:53:30 +0300

    initial commit, added all_users,user all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AllUsersComponent } from './admin/users/all-users/all-users.component';
+import { UserComponent } from './admin/users/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllUsersComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
