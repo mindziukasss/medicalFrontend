f1d7d0664d2e2651584e1444c1fdfe93698ae0eb
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-09-11 15:29:46 +0300

    added dashboard

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,15 @@
 import {Routes, RouterModule} from '@angular/router';
 import {AllUsersComponent} from './admin/users/all-users/all-users.component';
 import {ModuleWithProviders} from '@angular/core';
-
+import {DashboardComponent} from './admin/dashboard/dashboard.component';
 import {SigninComponent} from './admin/auth/signin/signin.component';
+import {SignoutComponent} from './admin/auth/signout/signout.component';
 import {AllPostsComponent} from './admin/posts/all-posts/all-posts.component';
 
 const APP_Routes:Routes = [
     {path: 'admin', component: SigninComponent},
+    {path: 'admin/signout', component: SignoutComponent},
+    {path: 'admin/dashboard', component: DashboardComponent},
     {path: 'admin/users', component: AllUsersComponent},
     {path: 'admin/posts', component: AllPostsComponent}
 ];
--- src/app/user.services.ts
+++ src/app/user.services.ts
@@ -2,16 +2,18 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/Rx';
 import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
 
 
 @Injectable()
 export class UserService {
-    constructor(private http: Http) {
+    constructor(private http: Http, private authService: AuthService) {
 
     }
 
     getUsers(): Observable<any> {
-        return this.http.get('http://medicback.dev/api/users').map((response: Response) => {
+        const token = this.authService.getToken();
+        return this.http.get('http://medicback.dev/api/users?token=' +token).map((response: Response) => {
             return response.json().users;
         });
     }
