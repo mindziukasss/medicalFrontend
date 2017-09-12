993b8f742f0a10bc84ff1232574c67f6c4f0bee5
Mindaugas Bernotas <mindziukasss@gmail.com>
2017-09-12 11:39:20 +0300

    start data to db

--- src/app/admin/users/add-user/add-user.component.ts
+++ src/app/admin/users/add-user/add-user.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {UserService} from '../../../user.services';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-add-user',
-  templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.css']
+    selector: 'app-add-user',
+    templateUrl: './add-user.component.html',
+    styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
 
-  constructor() { }
+    constructor(private userService: UserService,
+                private  router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    onCreate(form: NgForm) {
+        this.userService.createUser(
+            form.value.firs_name,
+            form.value.last_name,
+            form.value.email,
+            form.value.position,
+            form.value.password,
+            form.value.role_id,
+        ).subscribe(
+            response => this.router.navigate(['admin/user']),
+            error => console.log(error)
+        );
+    }
 }
--- src/app/user.services.ts
+++ src/app/user.services.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {Http, Response, Headers} from '@angular/http';
 import 'rxjs/Rx';
 import {Observable} from 'rxjs';
 import {AuthService} from './auth.service';
@@ -13,8 +13,32 @@ export class UserService {
 
     getUsers(): Observable<any> {
         const token = this.authService.getToken();
-        return this.http.get('http://medicback.dev/api/users?token=' +token).map((response: Response) => {
+        return this.http.get('http://medicback.dev/api/users?token=' + token).map((response: Response) => {
             return response.json().users;
         });
+    }
+
+    createUser(first_name: string,
+               last_name: string,
+               email: string,
+               position: string,
+               password: string,
+               role_id: number) {
+        const token = this.authService.getToken();
+        return this.http.post('http://medicback.dev/api/users/signin?token=' + token,
+            {
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                position: position,
+                password: password,
+                role_id: role_id
+            },
+            {headers: new Headers({'X-Requested-With': 'XMLHttpRequest'})}
+        ).map(
+            (response: Response) => {
+                return true;
+            }
+        );
     }
 }
